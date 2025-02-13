5248aa882be8918ae102f6ded4bbb0c46ca936ca
Yasinus <Yasinus@DESKTOP-29IUMJH>
2025-02-13 18:30:27 +0300

    Add project files.

---
+++ Bankamatik_Projesi/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Bankamatik_Projesi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
