5f491d39b2fd5ec0bfe3274aa2f9c0f689390c99
Yasinus <Yasinus@DESKTOP-29IUMJH>
2025-02-20 15:54:12 +0300

    update

--- Bankamatik_Projesi/Program.cs
+++ Bankamatik_Projesi/Program.cs
@@ -4,6 +4,8 @@ namespace Bankamatik_Projesi
 {
     internal class Program
     {
+        static int bakiye = 250;
+
         static void Main(string[] args)
         {
             IlkGiris();
@@ -49,9 +51,9 @@ namespace Bankamatik_Projesi
             do
             {
 
-           
-            Console.WriteLine(3-hak +1+ ". Hakkınız için Şifrenizi Giriniz )" );
-            string sifre = Console.ReadLine();
+
+                Console.WriteLine(3 - hak + 1 + ". Hakkınız için Şifrenizi Giriniz )");
+                string sifre = Console.ReadLine();
                 if (sifre == "ab18")
                 {
                     Console.WriteLine(" Ana Menüye Yönlendiriliyorsunuz...");
@@ -63,19 +65,147 @@ namespace Bankamatik_Projesi
                     Console.WriteLine("Yanlış şifre");
                     hak--;
                 }
-            } while (hak>0);
+            } while (hak > 0);
         }
 
         static void AnaMenu()
         {
             Console.WriteLine("Ana Menüye Hoşgeldiniz");
-            Console.WriteLine("Yapmak İstediğiniz İşlemi Seçiniz \n 1\t Para Çekme \n 2\t Para Yatırma ");
+            Console.WriteLine("Yapmak İstediğiniz İşlemi Seçiniz 1-Para Çekme 2-Para Yatırma 3-Para Transferleri  4-Eğitim Ödemeleri 5-Fatura Ödemeleri  6-Bilgi Güncellemesi ");
             string secim = Console.ReadLine();
+
 
             if (secim == "1")
             {
-                Console.WriteLine(  " Para Çekme Menüsüne Hoşgeldiniz");
+                Console.WriteLine(" Para Çekme Menüsüne Hoşgeldiniz");
+                ParaCekme();
+                return;
             }
+            else if (secim == "2")
+            {
+                Console.WriteLine("Para Yatırma Menüsüne Hoşgeldiniz");
+                ParaYatırma();
+                return;
+
+            }
+            else if (secim == "3")
+            {
+                Console.WriteLine("Para Transferleri Menüsüne Hoşgeldiniz");
+            }
+            else if (secim == "4")
+            {
+                Console.WriteLine("Eğitim Ödemeleri Menüsüne Hoşgeldiniz");
+            }
+            else if (secim == "5")
+            {
+                Console.WriteLine("Fatura Ödemeleri Menüsüne Hoşgeldiniz");
+            }
+            else if (secim == "6")
+            {
+                Console.WriteLine("Bilgi Güncelleme Menüsüne Hoşgeldiniz");
+            }
+            else
+            {
+                Console.WriteLine("Lütfen Seçenekler Doğrultusunda Seçim Yapınız");
+                AnaMenu();
+            }
+
+        }
+        static void ParaCekme()
+        {
+            Console.WriteLine("Çekmek İstediğiniz Tutarı Giriniz \nBakiyeniz:" + bakiye);
+            int CekmeTutar = Convert.ToInt32(Console.ReadLine());
+
+
+            if (CekmeTutar <= bakiye)
+            {
+                Console.WriteLine("Paranız Veriliyor, İşlem Başarılı");
+                Console.WriteLine("- **9:** Ana menüye dön. \r\n- **0:** Çıkış yap.  ");
+                if (Console.ReadLine() == "9")
+                {
+                    Console.WriteLine("Ana Menüye Yönlendiriliyorsunuz");
+                    bakiye = bakiye - CekmeTutar;
+                    AnaMenu();
+                }
+                else if (Console.ReadLine() == "0")
+                {
+                    Console.WriteLine("Çıkış Yapılıyor");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Yetersiz Bakiye");
+                Console.WriteLine("- **9:** Ana menüye dön. \r\n- **0:** Çıkış yap.  ");
+                if (Console.ReadLine() == "9")
+                {
+                    Console.WriteLine("Ana Menüye Yönlendiriliyorsunuz");
+                    bakiye = bakiye - CekmeTutar;
+                    AnaMenu();
+                }
+                else if (Console.ReadLine() == "0")
+                {
+                    Console.WriteLine("Çıkış Yapılıyor");
+                }
+            }
+
+
+        }
+        static void ParaYatırma()
+        {
+            Console.WriteLine("Kredi Kartına Para Yatırmak İçin 1'i Tuşlayınız" +
+                "\nHesaba Para Yatırmak İçin 2'yi Tuşlayınız \nBakiyeniz:" + bakiye);
+            int secim = Convert.ToInt32(Console.ReadLine());
+
+            if (secim == 1)
+            {
+
+                Console.WriteLine("12 Haneli Kredi Kartı Numaranızı Giriniz");
+                string kknumara = Console.ReadLine();
+                if (kknumara.Length == 12)
+                {
+                    Console.WriteLine("Yatırmak İstediğiniz Miktarı Giriniz");
+                    int kkmiktar = Convert.ToInt32(Console.ReadLine());
+                    if (kkmiktar <= bakiye)
+                    {
+                        Console.WriteLine("Yatırma İşleminiz Gerçekleşmiştir");
+                        bakiye = bakiye - kkmiktar;
+                        Console.WriteLine("- **9:** Ana menüye dön. \r\n- **0:** Çıkış yap.  ");
+                        if (Console.ReadLine() == "9")
+                        {
+                            Console.WriteLine("Ana Menüye Yönlendiriliyorsunuz");
+                            bakiye = bakiye - kkmiktar;
+                            AnaMenu();
+                        }
+                        else if (Console.ReadLine() == "0")
+                        {
+                            Console.WriteLine("Çıkış Yapılıyor");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bakiyeniz Yetersiz");
+                        ParaYatırma();
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Lütfen 12 Haneli Kredi Kartı Numaranızı Doğru Giriniz");
+                    ParaYatırma();
+                }
+            }
+            else if (secim == 2)
+            {
+                Console.WriteLine("Hesabınıza Yatırmak İstediğiniz Tutarı Giriniz ");
+                int hesapyatırma = Convert.ToInt32(Console.ReadLine());
+                bakiye = bakiye + hesapyatırma;
+                Console.WriteLine("Hesabınıza" + hesapyatırma + "TL Yatırdınız");
+                Console.WriteLine("- **9:** Ana menüye dön. \r\n- **0:** Çıkış yap.  ");
+
+            }
+
+
         }
 
 
